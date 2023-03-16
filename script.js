9c6ec71ee5fb17640306c0f91dd6189a785ae19a
Jan Korczak <jankorczak@Jans-MacBook-Air.local>
2023-03-16 10:29:23 +0100

    working on the burger

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+window.addEventListener ('load', () => {
+    window.addEventListener('scroll', function (e) {
+        if (window.pageYOffset > 100) {
+            document.querySelector("header").classList.add('is-scrolling');
+        } else {
+            document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
 
+    const menu_btn = document.querySelector(".hamburger");
+
+    menu_btn.addEventListener("click", () => {
+        menu_btn.classList.toggle('is-active');
+    })
+})
