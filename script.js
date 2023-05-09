2d57d2cbaeb7e5ebed13832e8f2e5e7cdf040543
Jan Korczak <jankorczak@Jans-MacBook-Air.local>
2023-05-09 22:23:17 +0200

    only cv, removed nav and hamburger

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const mobileNav = document.querySelector('ul');
+const mobileNav = document.querySelector('.top-nav ul');
 const burgerIcon = document.querySelector('.burger');
 
 burgerIcon.addEventListener("click", () => {
